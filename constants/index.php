2352e0df85b1e109b6664c9c57a1fb0f4e29297e
Sheraz Arif <sherazarifofficial@gmail.com>
2024-09-10 00:26:26 +0500

    added constants

---
+++ constants/index.php
@@ -0,0 +1,11 @@
+<?php
+const data = "sheraz";
+// const data = "tayyab"   cant change because it is constat
+echo data;
+?>
+
+<?php 
+$name = "sheraz";
+$name = "tayyab";  // can be changed because it is varible
+echo  $name;
+?>
