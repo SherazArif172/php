225aee965ab7a6d05f52da5d6da8c786109776d2
Sheraz Arif <sherazarifofficial@gmail.com>
2024-09-10 00:40:45 +0500

    data types

---
+++ dataTypes/index.php
@@ -0,0 +1,25 @@
+<?php 
+$str = "this is string";
+// echo var_dump($str);
+
+$num = 12;
+// echo var_dump($num);
+
+$floatt = 12.7;
+// echo var_dump($floatt);
+
+$bolean = false;
+// echo var_dump($bolean);
+
+$nll = NULL;
+// echo var_dump($nll);
+
+
+$arr = ["sheraz", 12];
+// echo var_dump($arr);
+
+// object class
+// resourses
+
+
+?>
