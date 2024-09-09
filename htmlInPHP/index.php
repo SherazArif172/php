58f93f1a6fb7ea8aeb2ac5e67d24a86af82e74bb
Sheraz Arif <sherazarifofficial@gmail.com>
2024-09-09 23:52:04 +0500

    adde html in php

---
+++ htmlInPHP/index.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "<h1 style=color:red;>sheraz<h1/>";
+echo "<h2 style=color:red;>sheraz<h1/>";
+$tayyab = "tayyab";
+
+?>
+
+<h1>
+    
+    <?php
+
+  echo "<h2 style=color:blue;>sheraz .. $tayyab<h1/>";
+    ?>
+</h1>
