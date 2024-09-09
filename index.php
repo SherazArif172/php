e25f1483dfb09b54ec3170ca3107b6698af7ae76
Sheraz Arif <sherazarifofficial@gmail.com>
2024-09-09 23:32:10 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "sheraz"; 
+$sheraz = 10;
+echo "<br>"; // Properly echo the <br> tag
+
+echo 10 * $sheraz; 
+echo "<br>";
+
+// echo 12 + $sheraz; // Uncomment and add semicolon if needed
+
+?>
