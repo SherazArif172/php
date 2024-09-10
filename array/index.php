b1e1a4450f6fe2adfcb4841f9b84a36c0e62e40e
Sheraz Arif <sherazarifofficial@gmail.com>
2024-09-10 20:06:42 +0500

    added array

---
+++ array/index.php
@@ -0,0 +1,16 @@
+<?php
+$sheraz = ["ali", "tayyab", "sheraz"];
+
+// echo $sheraz[0];
+// echo "<br>";
+// echo $sheraz[1];
+// echo "<br>";
+// echo $sheraz[2];
+
+for ($i=0; $i <count($sheraz) ; $i++) { 
+    # code...
+    echo $sheraz[$i];
+    echo "<br>";
+}
+
+?>
