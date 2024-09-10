325c98f49efc3a594cf573c8cb70117eda5139e9
Sheraz Arif <sherazarifofficial@gmail.com>
2024-09-10 19:33:41 +0500

    added variables

---
+++ variable function/index.php
@@ -0,0 +1,32 @@
+<?php
+// function tayyab() {
+//     echo "this is test"; 
+// }
+
+// $tayyab = "sheraz";
+
+// tayyab();
+
+
+// function as a prammerter
+
+function tayyab(){
+    echo "and tayyab";
+}
+
+sheraz($tayyab);
+
+
+$tayyab = "ty";
+
+
+function sheraz($tayyab){
+
+    echo "sheraz arif ";
+    tayyab();
+}
+
+
+
+
+?>
