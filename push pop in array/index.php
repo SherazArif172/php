3f3f737a760b1b66dd779df827a9cfbaa74d8490
Sheraz Arif <sherazarifofficial@gmail.com>
2024-09-10 23:21:54 +0500

    added push and pop

---
+++ push pop in array/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$sheraz = ["ali", "tayyab", "sheraz", "bazura"];
+
+
+// array_push($sheraz, "subhan"); 
+// array_pop($sheraz);    // will remove final name from array
+array_splice($sheraz,2);    // if use -2 it will remove final two names 
+
+print_r($sheraz)
+
+?>
