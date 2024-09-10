a2356a597baeb00806d03fe5cb86b9fa898b613e
Sheraz Arif <sherazarifofficial@gmail.com>
2024-09-10 13:49:20 +0500

    added logical operators

---
+++ logical operators/index.php
@@ -0,0 +1,20 @@
+<?php
+$a = 5;
+$b = 10;
+
+// if ($a == 5 and $b == 10) {
+//     echo "this is true";
+// }
+
+// if ($a == 50 || $b == 10) {
+//     echo "this is true";
+// }
+
+// if ($a == 50 xor $b == 10) {  // only one should be true 
+//     echo "this is true";
+// }
+
+// if ($a != 50) {  
+//     echo "this is true";
+// }
+?>
