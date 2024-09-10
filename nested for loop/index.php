030de36c4976916dc247cb8511b44e58c9099279
Sheraz Arif <sherazarifofficial@gmail.com>
2024-09-10 18:14:38 +0500

    added nested for loop

---
+++ nested for loop/index.php
@@ -0,0 +1,13 @@
+<?php
+for ($i=1; $i <4 ; $i++) { 
+    # code...
+    echo $i;
+    echo "<br>";
+
+    for ($j=1; $j < 4 ; $j++) { 
+        # code...
+        echo $j;
+        echo "<br>";
+    }
+}
+?>
