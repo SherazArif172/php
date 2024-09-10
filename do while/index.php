9019c462f05865436ce4f4b871829e0c1efdbc7d
Sheraz Arif <sherazarifofficial@gmail.com>
2024-09-10 18:45:13 +0500

    added goto

---
+++ do while/index.php
@@ -0,0 +1,9 @@
+<?php
+$ali = 0;
+do {
+    # code...
+    echo $ali;
+    echo "<br>";
+    $ali++;
+} while ($ali <= 10);
+?>
---
+++ goto/index.php
@@ -0,0 +1,36 @@
+<?php
+// $sh = 7;
+
+// if ($sh==7) {
+//     # code...
+//     goto jump;
+// }
+
+// $ty = "tayyab";
+// echo $ty;
+
+
+
+
+
+
+// jump:
+// echo "jumped";
+
+
+for ($i=0; $i <10 ; $i++) { 
+    # code...
+    echo $i; 
+    echo "<br>";
+
+    if ($i==6) {
+        # code...
+        goto outside;
+    }
+}
+
+
+outside:
+
+
+?>
