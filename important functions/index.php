f5b76528dc989a6b14d470a93d4453cddc2a55b3
Sheraz Arif <sherazarifofficial@gmail.com>
2024-09-10 19:50:44 +0500

    added important function

---
+++ important functions/index.php
@@ -0,0 +1,25 @@
+<?php
+$str = "sheraz arif";
+
+// echo strlen($str);   // to check how many characters are here 
+
+// echo var_dump($str);   // to check data type
+
+// echo date("m.d.y");
+
+// echo phpinfo()  // to check whole info about php
+
+// echo is_string($str)   // string or not
+
+// echo is_int($str)   // to check is integer or not
+
+// echo rand();        // to create random number
+
+// echo "before";
+// die();   // to stop the code
+// exit();  // to stop the code
+// echo "after";
+
+// echo substr($str,7)  // to skip first some characters
+
+?>
