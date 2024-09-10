e2dc58e2dcd82aa867f0542225cc54cc471b28d9
Sheraz Arif <sherazarifofficial@gmail.com>
2024-09-10 20:21:30 +0500

    addded associative arry

---
+++ associative arry/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$asso = ["name"=>"sheraz", "age"=>22];
+
+// echo $asso["name"];
+// echo "<br>";
+// echo $asso["age"];
+
+foreach ($asso as $data) {
+    # code...
+    echo $data;
+    echo "<br>";
+}
+
+?>
