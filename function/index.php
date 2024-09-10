81cbf46e370ec8b8bcff3517f8e8cc33a24306eb
Sheraz Arif <sherazarifofficial@gmail.com>
2024-09-10 18:53:49 +0500

    added function

---
+++ function/index.php
@@ -0,0 +1,23 @@
+<?php
+
+function tayyab(){
+    echo "details";
+}
+
+function sheraz() {
+    tayyab();
+    echo "<br>";
+    echo "<br>";
+
+
+    echo "sheraz";
+    echo "<br>";
+
+    echo "class 15";
+    echo "<br>";
+
+    echo "kotmomin";
+
+}
+sheraz();
+?>
