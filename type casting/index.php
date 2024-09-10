119ce18840725ca28126211f4f54b3a447ca42d8
Sheraz Arif <sherazarifofficial@gmail.com>
2024-09-10 23:59:13 +0500

    added type casting

---
+++ type casting/index.php
@@ -0,0 +1,19 @@
+<?php
+
+// $str = "ali";             // string to int
+// $str= (int) $str;
+// var_dump($str)
+
+// $int = 2;
+// $int= (string) $int;      // int to string
+// var_dump($int)
+
+// $int = 2;
+// $int= (boolean) $int;      // int to boolean
+// var_dump($int)
+
+// $bool = true;
+// $bool= (int) $bool;        // boolean to int
+// var_dump($bool)
+
+?>
